e23ad19a5d82de542f95b4e38720c7f3610d0d23
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-01-13 13:38:08 +0700

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          Hi from Nuxt PWA
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.headline {
+  margin-top: 300px;
+}
+</style>
